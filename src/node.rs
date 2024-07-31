ffe3ca59ce3cd2b66258d721a41198d58b95d901
Adam Colvin <adamcolv@icloud.com>
2024-07-31 09:15:25 +0100

    Support inserting into a tree with multiple levels
    The insert function will now traverse the tree looking for the correct place to insert a value.

--- src/node.rs
+++ src/node.rs
@@ -28,9 +28,20 @@ impl Node {
     }
 
     pub fn insert(&mut self, value: u32) {
-        self.keys.push(value);
-        if self.keys.len() >= self.max_keys {
-            self.split();
+        if self.children.len() == 0 {
+            self.keys.push(value);
+            self.keys.sort();
+            if self.keys.len() > self.max_keys {
+                self.split();
+            }
+        } else {
+            for child in self.children.iter_mut().rev() {
+                let lowest_key = child.keys.first().unwrap();
+                if value >= *lowest_key {
+                    child.insert(value);
+                    break;
+                }
+            }
         }
     }
 
@@ -86,6 +97,30 @@ mod tests {
     }
 
     #[test]
+    fn test_insert_into_child_node() {
+        let child_1 = Box::new(Node {
+            keys: vec![4, 5, 6],
+            ..Default::default()
+        });
+        let child_2 = Box::new(Node {
+            keys: vec![7, 8, 9],
+            ..Default::default()
+        });
+        let mut node = Node {
+            max_keys: 4,
+            keys: vec![1, 2, 3],
+            children: vec![child_1, child_2],
+        };
+        node.insert(10);
+        assert_eq!(node.keys, vec![1, 2, 3]);
+        assert_eq!(node.children.len(), 2);
+        let child_1 = node.children.first().unwrap();
+        assert_eq!(*child_1.keys, vec![4, 5, 6]);
+        let child_2 = node.children.last().unwrap();
+        assert_eq!(*child_2.keys, vec![7, 8, 9, 10]);
+    }
+
+    #[test]
     fn test_splitting_leaf_nodes() {
         let mut node = Node {
             max_keys: 4,
@@ -98,29 +133,6 @@ mod tests {
         assert_eq!(*expected_parent_key, 3);
         let expected_child_key = node.children.last().unwrap().keys.last().unwrap();
         assert_eq!(*expected_child_key, 5);
-    }
-
-    #[test]
-    fn test_splitting_nodes_with_children() {
-        let child_1 = Box::new(Node {
-            keys: vec![1, 2, 3],
-            ..Default::default()
-        });
-        let child_2 = Box::new(Node {
-            keys: vec![4, 5, 6],
-            ..Default::default()
-        });
-        let mut node = Node {
-            max_keys: 4,
-            keys: vec![1, 2, 3, 4],
-            children: vec![child_1, child_2],
-        };
-        node.insert(5);
-        assert_eq!(node.children.len(), 2);
-        let moved_child_1 = node.children.first().unwrap().children.first().unwrap();
-        assert_eq!(*moved_child_1.keys, vec![1, 2, 3]);
-        let moved_child_2 = node.children.last().unwrap().children.last().unwrap();
-        assert_eq!(*moved_child_2.keys, vec![4, 5, 6]);
     }
 
     #[test]
